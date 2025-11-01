d5d6dd5d3f547678bffc0e5e46df8b7b1561c2fe
HetPatel0 <hetp5852@gmail.com>
2025-11-01 11:59:35 +0530

    update

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -38,6 +38,7 @@ export async function AppSidebar() {
           }
 
         </SidebarGroupLabel>
+        //cannot
         {
            user && <SidebarCusGroupContent notes={notes} />
         }
